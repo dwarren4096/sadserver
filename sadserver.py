18f06aa0804e89a1df7e134b14ed84686133e161
Derek <dwarren4096@gmail.com>
2015-07-31 23:30:54 -0700

    added tweepy

--- sadserver.py
+++ sadserver.py
@@ -2,7 +2,7 @@
 
 # https://api.twitter.com/1.1/statuses/user_timeline.json?count=5&exclude_replies=true&include_rts=false&user_id=116568685&screen_name=sadserver
 
-#import tweepy
+import tweepy
 
 keyfile = open('api.key', 'r')
 apikey = keyfile.read()
@@ -16,7 +16,7 @@ URL = "https://api.twitter.com/1.1/statuses/user_timeline.json"
 PARAMS = ["user_id=116568685", "screen_name=sadserver", "count=5", "exclude_replies=true", "include_rts=false"]
 #PARAMS.append("")
 
-QUERY = '?'+PARAMS[0] #PARAMS concatenated, separated by & and prepended with a ?
+QUERY = '?'+PARAMS[0] 		#PARAMS concatenated, separated by & and prepended with a ?
 for i in range(1, len(PARAMS)):
   QUERY += '&'+PARAMS[i]
 
