a4463678e671cdd6f6ee6216c8a2219b9067c48b
Derek <dwarren4096@gmail.com>
2015-08-02 20:44:12 -0700

    added some tweepy stuff

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # sadserver
-Short little Python script that prints the latest tweet from @sadserver
+Short little Python script that prints the latest tweet from [@sadserver](https://twitter.com/sadserver)
--- sadserver.py
+++ sadserver.py
@@ -6,11 +6,20 @@ import tweepy
 
 keyfile = open('api.key', 'r')
 apikey = keyfile.read()
-print("API Key:", apikey)
 
 secretfile = open('api.secret', 'r')
 apisecret = secretfile.read()
-print("API Secret:", apisecret)
+
+atfile = open('access.token', 'r')
+access_token = atfile.read()
+
+asfile = open('access.secret', 'r')
+access_token_secret = asfile.read()
+
+auth = tweepy.OAuthHandler(apikey, apisecret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
 
 URL = "https://api.twitter.com/1.1/statuses/user_timeline.json"
 PARAMS = ["user_id=116568685", "screen_name=sadserver", "count=5", "exclude_replies=true", "include_rts=false"]
