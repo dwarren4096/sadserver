73fd87a7929d073ef8f4266d8db7eaf038e70b67
Derek Warren <dwarren4096@gmail.com>
2015-08-04 16:39:49 -0700

    This is completely broken and I likely won't get around to fixing it anytime soon. Oh well, at least I got to play around with Python strings

--- sadserver.py
+++ sadserver.py
@@ -2,6 +2,7 @@
 
 # https://api.twitter.com/1.1/statuses/user_timeline.json?count=5&exclude_replies=true&include_rts=false&user_id=116568685&screen_name=sadserver
 
+import json
 import tweepy
 
 keyfile = open('api.key', 'r')
@@ -20,13 +21,15 @@ auth = tweepy.OAuthHandler(apikey, apisecret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
+tweets = api.user_timeline("sadserver")
+print (tweets)
 
-URL = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-PARAMS = ["user_id=116568685", "screen_name=sadserver", "count=5", "exclude_replies=true", "include_rts=false"]
+#URL = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+#PARAMS = ["user_id=116568685", "screen_name=sadserver", "count=5", "exclude_replies=true", "include_rts=false"]
 #PARAMS.append("")
 
-QUERY = '?'+PARAMS[0] 		#PARAMS concatenated, separated by & and prepended with a ?
-for i in range(1, len(PARAMS)):
-  QUERY += '&'+PARAMS[i]
+#QUERY = '?'+PARAMS[0] 		#PARAMS concatenated, separated by & and prepended with a ?
+#for i in range(1, len(PARAMS)):
+#  QUERY += '&'+PARAMS[i]
 
-print(URL+QUERY)
+#print(URL+QUERY)
